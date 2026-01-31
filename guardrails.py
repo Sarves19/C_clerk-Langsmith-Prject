3ff0154da28df769aab7400081887bfa87bc93e6
Sarves19 <suresh.sarves19@gmail.com>
2026-01-31 13:22:54 +0530

    add comit: sdded a new filet

---
+++ guardrails.py
@@ -0,0 +1,12 @@
+
+def input_guardrail_node(state):
+
+    input_text = state.get("input")
+
+    blocked_words = ["bomb", "weapon", "drug", "hack", "explosive"]
+
+    for word in blocked_words:
+        if word in input_text:
+            return {"blocked" : True, "reason" : "Identified malicious intent"}
+
+    return {"blocked" : False}
